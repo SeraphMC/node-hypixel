47b30836abbce114a0a63a9e04ec15e112b5b128
Clemintina <git@seraph.si>
2025-04-08 19:52:23 +0200

    feat(bedwars): add `getBedwarsLevelInfoV2` for advanced prestige support
    Introduced a new helper function `getBedwarsLevelInfoV2` to support multicoloured BedWars levels above 1000 and ensure backwards compatibility. Added new interfaces and constants to handle advanced prestige formatting and display options. This update improves flexibility and enhances level representation.
    
    Signed-off-by: Clemintina <git@seraph.si>

--- src/helpers/BedwarsLevelInfo.ts
+++ src/helpers/BedwarsLevelInfo.ts
@@ -128,3 +128,430 @@ export function getBedwarsLevelInfo(
     levelInCurrentPrestige,
   };
 }
+
+/**
+ * Describes the results of the {@link getBedwarsLevelInfoV2} helper.
+ */
+export interface BedwarsLevelInfoV2 {
+  level: number;
+  prestige: number;
+  prestigeName: string;
+  prestigeColor: BedwarsPrestigeFormats;
+  prestigeColorHex: string;
+  levelInCurrentPrestige: number;
+}
+
+interface HighStarPrestige {
+  icon: string;
+  prestigeName: string;
+  colours: { value: string | number; hex: string }[];
+}
+
+interface BedwarsPrestigeFormats {
+  prestige: number;
+  format: MinecraftFormatting[];
+  bracketColour: {
+    beginning: MinecraftFormatting;
+    end: MinecraftFormatting;
+    star: MinecraftFormatting;
+  };
+}
+
+// TODO Add all prestige names and 3100-5000 levels
+const BEDWARS_PRESTIGES = [
+  {
+    prestige: 3000,
+    prestigeName: "",
+    format: [
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.GOLD,
+      MinecraftFormatting.GOLD,
+      MinecraftFormatting.RED,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.YELLOW,
+      end: MinecraftFormatting.DARK_RED,
+      star: MinecraftFormatting.DARK_RED,
+    },
+  },
+  {
+    prestige: 2900,
+    format: [
+      MinecraftFormatting.AQUA,
+      MinecraftFormatting.DARK_AQUA,
+      MinecraftFormatting.DARK_AQUA,
+      MinecraftFormatting.BLUE,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.AQUA,
+      end: MinecraftFormatting.BLUE,
+      star: MinecraftFormatting.BLUE,
+    },
+  },
+  {
+    prestige: 2800,
+    format: [
+      MinecraftFormatting.GREEN,
+      MinecraftFormatting.DARK_GREEN,
+      MinecraftFormatting.DARK_GREEN,
+      MinecraftFormatting.GOLD,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GREEN,
+      end: MinecraftFormatting.YELLOW,
+      star: MinecraftFormatting.GOLD,
+    },
+  },
+  {
+    prestige: 2700,
+    format: [
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.DARK_GRAY,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.YELLOW,
+      end: MinecraftFormatting.DARK_GRAY,
+      star: MinecraftFormatting.DARK_GRAY,
+    },
+  },
+  {
+    prestige: 2600,
+    format: [
+      MinecraftFormatting.DARK_RED,
+      MinecraftFormatting.RED,
+      MinecraftFormatting.RED,
+      MinecraftFormatting.LIGHT_PURPLE,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.DARK_RED,
+      end: MinecraftFormatting.DARK_PURPLE,
+      star: MinecraftFormatting.LIGHT_PURPLE,
+    },
+  },
+  {
+    prestige: 2500,
+    format: [
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.GREEN,
+      MinecraftFormatting.GREEN,
+      MinecraftFormatting.DARK_GREEN,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.WHITE,
+      end: MinecraftFormatting.DARK_GREEN,
+      star: MinecraftFormatting.DARK_GREEN,
+    },
+  },
+  {
+    prestige: 2400,
+    format: [
+      MinecraftFormatting.AQUA,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.GRAY,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.AQUA,
+      end: MinecraftFormatting.DARK_GRAY,
+      star: MinecraftFormatting.DARK_GRAY,
+    },
+  },
+  {
+    prestige: 2300,
+    format: [
+      MinecraftFormatting.DARK_PURPLE,
+      MinecraftFormatting.LIGHT_PURPLE,
+      MinecraftFormatting.LIGHT_PURPLE,
+      MinecraftFormatting.GOLD,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.DARK_PURPLE,
+      end: MinecraftFormatting.YELLOW,
+      star: MinecraftFormatting.YELLOW,
+    },
+  },
+  {
+    prestige: 2200,
+    format: [
+      MinecraftFormatting.GOLD,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.AQUA,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GOLD,
+      end: MinecraftFormatting.DARK_AQUA,
+      star: MinecraftFormatting.DARK_AQUA,
+    },
+  },
+  {
+    prestige: 2100,
+    format: [
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.GOLD,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.WHITE,
+      end: MinecraftFormatting.GOLD,
+      star: MinecraftFormatting.GOLD,
+    },
+  },
+  {
+    prestige: 2000,
+    format: [
+      MinecraftFormatting.GRAY,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.GRAY,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.DARK_GRAY,
+      end: MinecraftFormatting.DARK_GRAY,
+      star: MinecraftFormatting.DARK_GRAY,
+    },
+  },
+  {
+    prestige: 1900,
+    format: [
+      MinecraftFormatting.DARK_PURPLE,
+      MinecraftFormatting.DARK_PURPLE,
+      MinecraftFormatting.DARK_PURPLE,
+      MinecraftFormatting.DARK_PURPLE,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.DARK_GRAY,
+    },
+  },
+  {
+    prestige: 1800,
+    format: [
+      MinecraftFormatting.BLUE,
+      MinecraftFormatting.BLUE,
+      MinecraftFormatting.BLUE,
+      MinecraftFormatting.BLUE,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.DARK_GRAY,
+    },
+  },
+  {
+    prestige: 1700,
+    format: [
+      MinecraftFormatting.LIGHT_PURPLE,
+      MinecraftFormatting.LIGHT_PURPLE,
+      MinecraftFormatting.LIGHT_PURPLE,
+      MinecraftFormatting.LIGHT_PURPLE,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.DARK_PURPLE,
+    },
+  },
+  {
+    prestige: 1600,
+    format: [
+      MinecraftFormatting.DARK_RED,
+      MinecraftFormatting.DARK_RED,
+      MinecraftFormatting.DARK_RED,
+      MinecraftFormatting.DARK_RED,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.DARK_RED,
+    },
+  },
+  {
+    prestige: 1500,
+    format: [
+      MinecraftFormatting.DARK_AQUA,
+      MinecraftFormatting.DARK_AQUA,
+      MinecraftFormatting.DARK_AQUA,
+      MinecraftFormatting.DARK_AQUA,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.BLUE,
+    },
+  },
+  {
+    prestige: 1400,
+    format: [
+      MinecraftFormatting.DARK_GREEN,
+      MinecraftFormatting.DARK_GREEN,
+      MinecraftFormatting.DARK_GREEN,
+      MinecraftFormatting.DARK_GREEN,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.DARK_GREEN,
+    },
+  },
+  {
+    prestige: 1300,
+    format: [
+      MinecraftFormatting.AQUA,
+      MinecraftFormatting.AQUA,
+      MinecraftFormatting.AQUA,
+      MinecraftFormatting.AQUA,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.DARK_AQUA,
+    },
+  },
+  {
+    prestige: 1200,
+    format: [
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.YELLOW,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.GOLD,
+    },
+  },
+  {
+    prestige: 1100,
+    format: [
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.WHITE,
+      MinecraftFormatting.WHITE,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.GRAY,
+      end: MinecraftFormatting.GRAY,
+      star: MinecraftFormatting.DARK_GRAY,
+    },
+  },
+  {
+    prestige: 1000,
+    format: [
+      MinecraftFormatting.GOLD,
+      MinecraftFormatting.YELLOW,
+      MinecraftFormatting.GREEN,
+      MinecraftFormatting.AQUA,
+    ],
+    bracketColour: {
+      beginning: MinecraftFormatting.RED,
+      end: MinecraftFormatting.DARK_PURPLE,
+      star: MinecraftFormatting.LIGHT_PURPLE,
+    },
+  },
+];
+
+/**
+ * Calculates the BedWars prestige and level of a player and returns a {@link BedwarsLevelInfoV2} interface.
+ * This has been versioned as V2 to ensure backwards compatability with the older function. This function enables support for multicoloured levels above 1000.
+ * @category Helper
+ */
+export const getBedwarsLevelInfoV2 = (
+  data: BedwarsLevelInfo,
+  options: { displayBrackets?: boolean; displayIcon?: boolean } = {}
+) => {
+  const colourFormat = BEDWARS_PRESTIGES;
+  const { level, prestigeColorHex } = data;
+  const { displayBrackets = false, displayIcon = false } = options;
+
+  const highStarPrestige: HighStarPrestige = {
+    icon: "✫",
+    prestigeName: "",
+    colours: [],
+  };
+
+  if (level >= 1100 && level < 2100) highStarPrestige.icon = "✪";
+  if (level >= 2100) highStarPrestige.icon = "❀";
+
+  if (level >= 1000) {
+    colourFormat.forEach(
+      ({ format, bracketColour, prestige, prestigeName }) => {
+        if (prestige <= level) {
+          const levelSplit = Array.from(level.toString()).map(Number);
+
+          if (displayBrackets) {
+            highStarPrestige.colours.push({
+              value: "[",
+              hex:
+                MinecraftColorAsHex[
+                  bracketColour.beginning as keyof typeof MinecraftColorAsHex
+                ],
+            });
+          }
+
+          for (let i = 0; i < levelSplit.length; i++) {
+            highStarPrestige.colours.push({
+              value: levelSplit[i],
+              hex:
+                MinecraftColorAsHex[
+                  format[i] as keyof typeof MinecraftColorAsHex
+                ],
+            });
+          }
+
+          if (displayIcon) {
+            highStarPrestige.colours.push({
+              value: highStarPrestige.icon,
+              hex:
+                highStarPrestige.colours[highStarPrestige.colours.length - 1]
+                  .hex,
+            });
+          }
+
+          if (displayBrackets) {
+            highStarPrestige.colours.push({
+              value: "]",
+              hex:
+                MinecraftColorAsHex[
+                  bracketColour.end as keyof typeof MinecraftColorAsHex
+                ],
+            });
+          }
+
+          highStarPrestige.prestigeName = prestigeName;
+          return highStarPrestige;
+        }
+        return null;
+      }
+    );
+  } else {
+    if (displayBrackets) {
+      highStarPrestige.colours.push({ value: "[", hex: prestigeColorHex });
+    }
+    highStarPrestige.colours.push({ value: level, hex: prestigeColorHex });
+    if (displayIcon) {
+      highStarPrestige.colours.push({
+        value: highStarPrestige.icon,
+        hex: prestigeColorHex,
+      });
+    }
+    if (displayBrackets) {
+      highStarPrestige.colours.push({ value: "]", hex: prestigeColorHex });
+    }
+    return highStarPrestige;
+  }
+
+  return {
+    level,
+    prestige: Math.round(level / 1000) * 10,
+    prestigeName: highStarPrestige.prestigeName,
+    prestigeColor: highStarPrestige.colours,
+    prestigeColorHex: highStarPrestige.colours.map((colour) => colour.hex),
+    levelInCurrentPrestige: data.levelInCurrentPrestige,
+  };
+};
